e5bf6e90cdc4b3e4afd2080d9a2f30fc0ce68da0
Giorgi <giorgimsxrikadze@gmail.com>
2026-07-18 01:02:32 +0400

    created LogAnalyzer class.implemented the input output path check.Validated input and output directory

---
+++ src/LogAnalyzer.java
@@ -0,0 +1,57 @@
+import java.io.File;
+
+public class LogAnalyzer{
+    public static void main(String[] args){
+        //user may seek for help(understanding program)
+        if(args.length>0 && (args[0].equals("-h") || args[0].equals("--help"))){
+            printUsage();
+            System.exit(0);
+        }
+
+        String inputPath;
+        String outputPath;
+
+        if (args.length == 2) {
+            inputPath = args[0];
+            outputPath = args[1];
+        }else{
+            inputPath="logs/system.log";
+            outputPath="reports/summary_report.txt";
+            System.out.println("Default arguments used");
+        }
+
+
+        File inputFile=new File(inputPath);
+        File outputFile=new File(outputPath);
+
+        //input validation
+        if(!(inputFile.exists() || inputFile.isFile())){
+            System.err.println("Path of input log or log File does not exist");
+            System.exit(1);
+        }
+
+        //output validation
+        File parentDir=outputFile.getParentFile();
+        if( parentDir!=null && !parentDir.exists()){
+            if(!parentDir.mkdir()){
+                System.err.println("Can't create output file directory!");
+                System.exit(1);
+            }
+        }
+
+
+
+
+    }
+
+
+    private static void printUsage(){
+        System.out.println("Secure OS-Level Log Analyzer");
+        System.out.println("Usage:");
+        System.out.println("  java LogAnalyzer [input_log_file] [output_report_file]");
+        System.out.println();
+        System.out.println("Defaults:");
+        System.out.println("  input_log_file:     logs/server.log");
+        System.out.println("  output_report_file: reports/summary_report.txt");
+    }
+}
