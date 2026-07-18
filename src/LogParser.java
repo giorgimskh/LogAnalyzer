be878602910f2c610749e6f39f48b6f551b11022
Giorgi <giorgimsxrikadze@gmail.com>
2026-07-18 22:57:50 +0400

    implemented logParser

--- src/LogParser.java
+++ src/LogParser.java
@@ -1,9 +1,8 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class LogParser {
-    public class AnalysisResult{
+    public class AnalysisResult {
         public final long totalLines;
         public final long infoCount;
         public final long warningCount;
@@ -21,6 +20,35 @@ public class LogParser {
 
     }
 
+    public AnalysisResult logParser(File inputFile, ReportWriter reportWriter) {
+        long totalLines = 0;
+        long infoCount = 0;
+        long warningCount = 0;
+        long errorCount = 0;
+        long unclassifiedCount=0;
 
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8));
+            String line;
+
+            while((line = reader.readLine()) !=null){
+                totalLines++;
+
+                line=line.toUpperCase();
+
+                if(line.contains("INFO:")) {
+                    infoCount++;
+                }else if(line.contains("WARNING:")){
+                    warningCount++;
+                }else if(line.contains("EROR:")){
+                    errorCount++;
+                    reportWriter.writeErrorLine(line);
+                }else{
+                    unclassifiedCount++;
+                }
+            }
+
+            return new AnalysisResult(totalLines,infoCount,warningCount,errorCount,unclassifiedCount);
+    }
 
 }
