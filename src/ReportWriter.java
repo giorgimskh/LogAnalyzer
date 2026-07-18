b49713fb970b6f87b4455dcabb27161f9e54bc63
Giorgi <giorgimsxrikadze@gmail.com>
2026-07-19 00:21:14 +0400

    implemeted ReportWriter class with writeErrorLine method and constructor

--- src/ReportWriter.java
+++ src/ReportWriter.java
@@ -1,2 +1,39 @@
-public class ReportWriter {
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class ReportWriter implements AutoCloseable{
+    private final FileOutputStream fos;
+    private final BufferedWriter writer;
+
+    public ReportWriter(File outputFile) throws FileNotFoundException {
+        this.fos=new FileOutputStream(outputFile);
+
+        //The writer keeps text inside a quick-access temporary memory block
+        //OutputStreamWriter(..., StandardCharsets.UTF_8) (The Translator):
+        // It translates your Java strings into raw binary data (0s and 1s) using the UTF-8 character map.
+        //This is the low-level utility that takes those raw bytes from the translator and
+        // delivers them straight to the operating system's file system handler.
+        this.writer = new BufferedWriter(new OutputStreamWriter((fos), StandardCharsets.UTF_8));
+    }
+
+    public  void writeErrorLine(String line) throws IOException {
+        writer.write("[CRITICAL ERROR] -> " + line);
+
+        //Empties Java's internal memory buffer down to the OS cache.
+        //if power goes off,data lost still
+        writer.flush();
+
+        try{
+            // 2. Grabs the low-level OS hook for this exact file.
+            FileDescriptor fd=fos.getFD();
+
+            // 3. Forces the Operating System to pause and physically write the cache to the disk
+            //data is safe now
+            fd.sync();
+        }catch (SyncFailedException e){
+            System.err.println("⚠️ Warning: Hardware disk sync failed (filesystem may not support sync): " + e.getMessage());
+        }
+    }
+
+
 }
